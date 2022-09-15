e545758ce30152f22ed3f1522e71ea9c9134f557
revalina19 <113223272+revalina19@users.noreply.github.com>
2022-09-15 18:54:52 +0700

    Add files via upload

---
+++ menghitung v balok.py
@@ -0,0 +1,6 @@
+# MENGHITUNG VOLUME BALOK
+p= float(input ("masukkan panjang balok "))
+l= float(input ("masukkan lebar balok "))
+t= float(input ("masukkan tinggi balok "))
+VOLUME= p*l*t
+print("volume balok adalah : " + str(VOLUME))
